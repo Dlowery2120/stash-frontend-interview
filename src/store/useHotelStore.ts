daef9b784898d94d2b280361f2c42399bd2d8f69
Devante Lowery <lay29@txstate.edu>
2025-04-23 19:08:56 -0500

    new Destination type and addition of query

--- src/store/useHotelStore.ts
+++ src/store/useHotelStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
 import { fetchHotels } from '../utils/fetchHotels'
-type Hotel = {
+
+export type Hotel = {
   id: number
   name: string
   city: string
@@ -9,24 +10,35 @@ type Hotel = {
   has_member_rate: boolean
 }
 
+export type Destination = {
+  city: string
+}
+
 type State = {
   hotels: Hotel[]
+  destinations: string[]
   loading: boolean
+  query: string
+  setQuery: (q: string) => void
   loadHotels: () => Promise<void>
 }
 
 export const useHotelStore = create<State>((set) => ({
   hotels: [],
+  destinations: [],
   loading: false,
+  query: '',
+  setQuery: (q) => set({ query: q }),
   loadHotels: async () => {
     set({ loading: true })
     try {
-      const data = await fetchHotels()
-      set({ hotels: data })
+      const data = await fetchHotels() as Hotel[] 
+      const cities = Array.from(new Set(data.map((h: Hotel) => h.city)))
+      set({ hotels: data, destinations: cities })
     } catch (err) {
       console.error(err)
     } finally {
       set({ loading: false })
     }
-  }
+  }  
 }))
