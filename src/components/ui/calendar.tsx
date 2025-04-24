d4e94cb8c7c268a81ef53e60475c333da10545f4
Devante Lowery <lay29@txstate.edu>
2025-04-24 04:47:30 -0500

    date picker color change

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -43,17 +43,17 @@ function Calendar({
           "size-8 p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_start:
-          "day-range-start aria-selected:bg-primary aria-selected:text-primary-foreground",
+          "day-range-start aria-selected:bg-primary aria-selected:text-[#e08923]",
         day_range_end:
-          "day-range-end aria-selected:bg-primary aria-selected:text-primary-foreground",
+          "day-range-end aria-selected:bg-[#e08923] aria-selected:text-[#e08923]",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-[#e08923]focus:text-[#e08923]",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "aria-selected:bg-[#e08923] aria-selected:text-[#e08923]",
         day_hidden: "invisible",
         ...classNames,
       }}
