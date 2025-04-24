b7f6151928762505f6b7e873eb69bcaaf72afa2f
Devante Lowery <lay29@txstate.edu>
2025-04-24 01:13:28 -0500

    fix (style): navbar width and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 import HotelsIndex from './pages/HotelsIndex';
 import HotelDetails from './pages/HotelDetails';
 import DestinationResults from './pages/DestinationResults';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <div className='min-h-screen'>
-      <Routes>
-        <Route path="/" element={<HotelsIndex />} />
-        <Route path="/hotel/:id" element={<HotelDetails />} />
-        <Route path="/travel/destination/:city" element={<DestinationResults />} />
-      </Routes>
+    <div className="min-h-screen w-full min-w-screen">
+      <Navbar />
+
+      <main className="flex justify-center px-4">
+        <div className="w-full max-w-7xl">
+          <Routes>
+            <Route path="/" element={<HotelsIndex />} />
+            <Route path="/hotel/:id" element={<HotelDetails />} />
+            <Route path="/travel/destination/:city" element={<DestinationResults />} />
+          </Routes>
+        </div>
+      </main>
     </div>
   );
 }
